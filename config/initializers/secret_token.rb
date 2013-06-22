b6f2c71f4396d4804d417affe133b0da3c7c9ef0
Chandresh Pant <cp@cpant.in>
2013-06-22 05:51:03 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cart::Application.config.secret_token = '73035f9c2baec68a2526e41528633ae811a5809e42326dc357af872f34cc3354aa2f549a2660c3e61607c480fab234f4c8183f86842095a2e683b6474ade635e'

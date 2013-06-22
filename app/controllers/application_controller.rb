6f139d92afe5085ca0658124a1178dc46f867aa0
Chandresh Pant <cp@cpant.in>
2013-06-22 06:59:49 +0530

    current_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_cart
+    if session[:cart_id].present?
+      @cart ||= Basket.find(session[:cart_id])
+    else
+      @cart = Basket.create
+      session[:cart_id] = @cart.id
+    end
+    @cart
+  end
+
+  helper_method :current_cart
+
 end

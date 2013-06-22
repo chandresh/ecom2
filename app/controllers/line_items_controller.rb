4790f14697d8407e4f5ea150ed7b8acff764925f
Chandresh Pant <cp@cpant.in>
2013-06-22 20:36:37 +0530

    refactored the line_items controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -6,22 +6,8 @@ class LineItemsController < ApplicationController
   end
 
   def create
-    # grab the current_cart
     product = Product.find(params[:product_id])
-
-    ## first check if the cart already has the product
-    @cart_line_items = current_cart.line_items.where(product_id: product.id)
-    if @cart_line_items.count > 0
-      @line_item = @cart_line_items.last
-      @line_item.quantity += 1
-    else
-      @line_item = current_cart.line_items.new(product_id: product.id, quantity: 1, price: product.price)
-    end
-
-    if @line_item.save
-      redirect_to current_cart, notice: "Product Added successfully."
-    else
-      redirect_to root_url, notice: "Some error, please try adding product again."
-    end
+    current_cart.add_line_item_for(product)
+    redirect_to current_cart, notice: "Product added successfully."
   end
 end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -7,4 +7,15 @@ class Basket < ActiveRecord::Base
     line_items.map(&:total).inject(:+)
   end
 
+  def add_line_item_for(product)
+    product_line_items = line_items.where(product_id: product.id)
+    if product_line_items.count > 0
+      line_item = product_line_items.last
+      line_item.quantity += 1
+    else
+      line_item = line_items.new(product_id: product.id, quantity: 1, price: product.price)
+    end
+    line_item.save
+  end
+
 end
